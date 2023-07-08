5cff893bc3e4c26d53c7bba0dce5fb88d18ef1a5
Paulselikhov <selikhov.pasha@yandex..ru>
2023-07-08 13:06:28 +0300

    Сгенерировал шаблоны. Настроил роуты

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibraryComponent } from './components/library/library.component';
+import { BookComponent } from './components/book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LibraryComponent},
+  {path: 'book/:id', component: BookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './components/book/book.component';
+import { LibraryComponent } from './components/library/library.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
